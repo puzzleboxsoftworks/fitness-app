5ba5b7c9b7be879ea012f29e46223311047bb2ec
The Keeper <156603131+puzzleboxsoftworks@users.noreply.github.com>
2026-03-22 15:12:11 -0400

    Update main.js

---
+++ js/main.js
@@ -0,0 +1,26 @@
+// Load exercises and programs
+let exercisesData = [];
+let programsData = [];
+
+fetch('_data/exercises.json')
+  .then(res=>res.json())
+  .then(data=>exercisesData=data);
+
+fetch('_data/programs.json')
+  .then(res=>res.json())
+  .then(data=>programsData=data);
+
+function showProgram(programName){
+    const program = programsData.find(p=>p.name===programName);
+    const container = document.getElementById('workoutList');
+    container.innerHTML = '';
+    program.days.forEach(day=>{
+        let div = document.createElement('div');
+        div.innerHTML = `<h3>Day ${day.day}</h3>`;
+        day.exercises.forEach(exId=>{
+            const ex = exercisesData.find(e=>e.id===exId);
+            div.innerHTML += `<p>${ex.name} - ${ex.duration}s | ${ex.description}</p>`;
+        });
+        container.appendChild(div);
+    });
+}
